2f2c9b35fc37a53380e602ed6ba3d435e4196b32
xuyuanreporter <127954188+xuyuanreporter@users.noreply.github.com>
2025-08-27 15:52:30 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '1339d12e-327f-4dc7-b8bf-c0e603de158f';
+let userID = 'bf0369d0-c785-4055-a268-a43e37fa4d14';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
